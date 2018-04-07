7b323552b42a321e1a62096a68834fbbf936c136
Tyler Berry <tyler@berryac.com>
2018-04-07 02:42:23 +0000

    Modularize

---
+++ consts.py
@@ -0,0 +1,5 @@
+REQUEST_HEADER = {
+    'User-Agent': ('Mozilla/5.0 (Windows NT 6.1; WOW64) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/43.0.2357.134 Safari/537.36')
+}
