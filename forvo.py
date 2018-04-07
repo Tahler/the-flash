357ca051917e32790656ae480c2a6dcbb578e838
Tyler Berry <tyler@berryac.com>
2018-04-07 19:08:22 +0000

    Can scrape and save pronunciations from Forvo

---
+++ forvo.py
@@ -0,0 +1,55 @@
+import base64
+import bs4
+import re
+import requests
+from typing import Iterable, Optional
+
+import soups
+
+PATTERN = re.compile('Play\((.*)\)')
+
+
+def _get_query_url(query: str) -> str:
+    encoded_query = '%20'.join(query.split())
+    return 'https://forvo.com/search/{}'.format(encoded_query)
+
+
+def _extract_play_id(on_click_play_call: str) -> Optional[str]:
+    """Extracts the fifth argument to the Play() call."""
+    match = PATTERN.search(on_click_play_call)
+    if match:
+        args_str = match.group(1)
+        args = args_str.split(',')
+        play_id = args[4]
+    else:
+        play_id = None
+    return play_id
+
+
+def _get_mp3_url(path: str) -> str:
+    return 'https://audio00.forvo.com/audios/mp3/{}'.format(path)
+
+
+def _extract_mp3_urls(soup: bs4.BeautifulSoup) -> Iterable[str]:
+    play_spans = soup.find('li', 'list-words').find_all('span', 'play')
+    on_clicks = (span['onclick'] for span in play_spans)
+    play_ids = (_extract_play_id(on_click) for on_click in on_clicks)
+    byte_str_paths = (base64.b64decode(play_id) for play_id in play_ids
+             if play_id is not None)
+    paths = (byte_str.decode('utf-8') for byte_str in byte_str_paths)
+    urls = (_get_mp3_url(path) for path in paths)
+    return urls
+
+
+def _get_mp3(url: str) -> bytes:
+    response = requests.get(url)
+    return response.content
+
+
+def query(query: str) -> Iterable[bytes]:
+    """Returns a generator of raw MP3 data for query from Forvo."""
+    url = _get_query_url(query)
+    soup = soups.get(url)
+    urls = _extract_mp3_urls(soup)
+    mp3s = (_get_mp3(url) for url in urls)
+    return mp3s
--- main.py
+++ main.py
@@ -5,50 +5,66 @@ import itertools
 import os
 from typing import Iterable, Tuple
 
+import forvo
 import google_images
 
 
-def _save_image(raw_image: bytes,
-                directory: str,
-                file_name: str,
-                extension: str = 'jpg') -> None:
-    path = os.path.join(directory, file_name + '.' + extension)
+def _save_bin_file(content: bytes, directory: str, file_name: str) -> None:
+    path = os.path.join(directory, file_name)
     with open(path, 'wb+') as f:
-        f.write(raw_image)
+        f.write(content)
 
 
-def _download_images_to_dir(img_ext_tuples: Iterable[Tuple[str, str]],
-                            directory: str, num_images: int) -> None:
+def _save_imgs_to_dir(img_ext_tuples: Iterable[Tuple[str, str]],
+                      directory: str, limit: int) -> None:
     if not os.path.exists(directory):
         os.makedirs(directory)
-    limit_img_ext_tuples = itertools.islice(img_ext_tuples, num_images)
-    for i, (img, ext) in enumerate(limit_img_ext_tuples):
-        _save_image(img, directory, str(i), ext)
+    img_ext_tuples_slice = itertools.islice(img_ext_tuples, limit)
+    for i, (img, ext) in enumerate(img_ext_tuples_slice):
+        file_name = '{}.{}'.format(i, ext)
+        _save_bin_file(img, directory, file_name)
 
 
-def _run(query: str, directory: str, num_images: int = 100) -> None:
+def _save_mp3s_to_dir(mp3s: Iterable[bytes], directory: str,
+                          limit: int) -> None:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    mp3s_slice = itertools.islice(mp3s, limit)
+    for i, mp3 in enumerate(mp3s_slice):
+        file_name = '{}.mp3'.format(i)
+        _save_bin_file(mp3, directory, file_name)
+
+
+def _run(query: str,
+         directory: str,
+         num_images: int = 5,
+         num_pronunciations: int = 5) -> None:
     img_ext_tuples = google_images.query(query)
-    _download_images_to_dir(img_ext_tuples, directory, num_images)
+    _save_imgs_to_dir(img_ext_tuples, directory, num_images)
+
+    mp3s = forvo.query(query)
+    _save_mp3s_to_dir(mp3s, directory, num_pronunciations)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description='Scrape and save images from Google Images')
+        description='Scrape the web to help make Anki flash cards.')
     parser.add_argument('search', type=str, help='the search query')
     parser.add_argument(
-        '-n',
-        '--num_images',
+        '--num_images', default=1, type=int, help='number of images to save')
+    parser.add_argument(
+        '--num_pronunciations',
         default=1,
         type=int,
-        help='number of images to save')
+        help='number of pronunciations to save')
     parser.add_argument(
         '-d',
         '--directory',
-        default='google_images',
+        default='out',
         type=str,
-        help='directory to contain the images')
+        help='directory to contain the scraped results')
     args = parser.parse_args()
-    _run(args.search, args.directory, args.num_images)
+    _run(args.search, args.directory, args.num_images, args.num_pronunciations)
 
 
 if __name__ == '__main__':
