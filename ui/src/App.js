c8b947a851dea59731236292d9dd010b6eee546e
Tyler Berry <tyler@berryac.com>
2019-03-19 22:09:00 -0700

    Add components for querying /images/google

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -20,9 +20,58 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Search></Search>
       </div>
     );
   }
 }
 
-export default App;
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      imgUrls: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({query: event.target.value});
+  }
+
+  query(query) {
+    const path = encodeURIComponent(query);
+    const url = `http://localhost:5000/images/google/${path}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(imgUrls => this.setState({imgUrls}));
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" name="query" value={this.state.query} onChange={this.handleChange} />
+        <button onClick={() => this.query(this.state.query)}>Submit</button>
+        <Images urls={this.state.imgUrls} />
+      </div>
+    );
+  }
+}
+
+class Images extends Component {
+  static defaultProps = {
+    urls: [],
+  };
+
+  render() {
+    const imgs = this.props.urls.map(url =>
+        <img src={url} alt="" className="result-img" key={url}></img>)
+    return (
+      <div>
+        {imgs}
+      </div>
+    );
+  }
+}
