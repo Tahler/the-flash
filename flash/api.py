9b1a3aad9d3e13a7138f61cddd2b122f6e81bd86
Tyler Berry <tyler@berryac.com>
2019-03-21 20:08:53 -0700

    Serve static files for dev

--- flash/api.py
+++ flash/api.py
@@ -14,17 +14,32 @@ def json_cors(*args, **kwargs) -> flask.Response:
 
 @app.route('/audio/forvo/<query>')
 def query_audio_forvo(query: str) -> flask.Response:
-    urls = list(forvo.query(query))
+    # urls = list(forvo.query(query))[:3]
+    urls = [
+      'http://localhost:5000/static/montaña1.mp3',
+      'http://localhost:5000/static/montaña2.mp3',
+      'http://localhost:5000/static/montaña3.mp3',
+    ]
     return json_cors(urls)
 
 
 @app.route('/images/google/<query>')
 def query_images_google(query: str) -> flask.Response:
-    urls = list(google_images.query(query))
+    # urls = list(google_images.query(query))[:3]
+    urls = [
+      'http://localhost:5000/static/montaña1.jpg',
+      'http://localhost:5000/static/montaña2.jpg',
+      'http://localhost:5000/static/montaña3.jpg',
+    ]
     return json_cors(urls)
 
 
 @app.route('/examples/tatoeba/<query>')
 def query_examples_tatoeba(query: str) -> flask.Response:
-    examples = [s for s, _ in sentences.query(query)]
+    # examples = [s for s, _ in sentences.query(query)][:3]
+    examples = [
+      'Escalo montañas.',
+      'Mira esa montaña.',
+      'Mirá esa montaña.',
+    ]
     return json_cors(examples)
