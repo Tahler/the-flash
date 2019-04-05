3fe5824e5daebaf57a1e1fca8bbfc075011a322c
Tyler Berry <tyler@berryac.com>
2019-04-05 11:19:30 -0700

    Add MultiCardCreator.js which cycles through words

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Search from './Search';
+import MultiCardCreator from './MultiCardCreator';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Search></Search>
+        <MultiCardCreator words={['montaña', 'casa', 'cabrero']}></MultiCardCreator>
       </div>
     );
   }
---
+++ ui/src/MultiCardCreator.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { query } from './shared/query';
+import FlashCardCreator from './FlashCardCreator';
+
+export default class MultiCardCreator extends Component {
+  static defaultProps = {
+    words: [],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      flashCards: [],
+      wordIndex: -1,
+      word: undefined,
+      imgUrls: [],
+      mp3Urls: [],
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.nextWord();
+  }
+
+  async nextWord() {
+    const nextWordIndex = this.state.wordIndex + 1;
+    const nextWord = this.props.words[nextWordIndex];
+    const {
+      imgUrls,
+      mp3Urls,
+    } = await query(nextWord);
+    this.setState({
+      wordIndex: nextWordIndex,
+      word: nextWord,
+      imgUrls,
+      mp3Urls,
+    });
+  }
+
+  async onSubmit(flashCard) {
+    const {
+      flashCards,
+      word,
+    } = this.state;
+
+    const completeFlashCard = {
+      word,
+      ...flashCard,
+    };
+    this.setState({
+      flashCards: [
+        ...flashCards,
+        completeFlashCard,
+      ],
+    });
+    await this.nextWord();
+  }
+
+  render() {
+    const {
+      imgUrls,
+      mp3Urls,
+    } = this.state;
+    return (
+      <FlashCardCreator
+          imgUrls={imgUrls}
+          mp3Urls={mp3Urls}
+          onSubmit={this.onSubmit}
+      />
+    );
+  }
+}
