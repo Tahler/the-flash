0dbd8ca3050b05ad28efe832bfe4b9049ece5889
Tyler Berry <tyler@berryac.com>
2019-04-05 11:57:02 -0700

    Transition from word list to flash card creation

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import MultiCardCreator from './MultiCardCreator';
 import WordListEntry from './WordListEntry';
 import './App.css';
 
+const stage = {
+  wordListEntry: 0,
+  createFlashCards: 1,
+};
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stage: stage.wordListEntry,
+      words: [],
+    };
+    this.receiveWords = this.receiveWords.bind(this);
+  }
+
+  receiveWords(words) {
+    this.setState({
+      stage: stage.createFlashCards,
+      words,
+    })
+  }
+
   render() {
+    let content;
+    switch (this.state.stage) {
+      case stage.wordListEntry:
+        content = <WordListEntry onSubmit={this.receiveWords}></WordListEntry>;
+        break;
+      case stage.createFlashCards:
+        content = <MultiCardCreator words={this.state.words}></MultiCardCreator>
+        break;
+      default:
+        throw new Error('impossible');
+    }
     return (
       <div className="App">
-        <WordListEntry onSubmit={words => console.log(words)}></WordListEntry>
+        {content}
       </div>
     );
   }
