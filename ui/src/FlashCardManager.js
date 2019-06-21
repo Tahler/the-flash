21a80f998ed8c1fc5e6b3a1be1bf3c1f841176d2
Tyler Berry <tyler@berryac.com>
2019-06-20 23:30:32 -0700

    WIP download zip

--- ui/src/FlashCardManager.js
+++ ui/src/FlashCardManager.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import JSZip from 'jszip';
 import { loadFlashCards } from './shared/storage';
 
 export default class FlashCardManager extends Component {
@@ -12,19 +13,59 @@ export default class FlashCardManager extends Component {
     this.download = this.download.bind(this);
   }
 
-  download() {
-    const header = 'name,images,audio,examples';
-    const cardRows = this.state.flashCards.map(card => {
-      const imgs = card.imageUrls.map(url => `<img src="${url}">`).join(';');
-      const sound = `[sound:${card.audioUrl}]`;
-      const examples = card.examples.join(';');
-      const fields = [card.word, imgs, sound, examples]
-          .map(f => `"${f}"`);
-      return fields.join(',');
+  extractExtension(url) {
+    const splits = url.split('.');
+    return splits[splits.length - 1];
+  }
+
+  async download() {
+    const blobbedCards = await this.state.flashCards.map(async card => {
+      const namedImageBlobs = await card.imageUrls.map(async (url, i) => {
+        const ext = this.extractExtension(url);
+        // TODO: parallelize these fetches
+        const blob = await fetch(url, { mode: 'no-cors' }).then(r => r.blob());
+        // TODO: enforce unique words
+        return [`${card.word}-${i}.${ext}`, blob];
+      });
+      // TODO: this is returning promises
+      console.log(namedImageBlobs)
+      const audioExt = this.extractExtension(card.audioUrl);
+      const audioBlob = await fetch(card.audioUrl, { mode: 'no-cors' }).then(r => r.blob());
+      const namedAudioBlob = [`${card.word}.${audioExt}`, audioBlob];
+      return {
+        word: card.word,
+        examples: card.examples,
+        namedImageBlobs,
+        namedAudioBlob,
+      };
     });
-    const rows = [header, ...cardRows];
-    const content = rows.join('\n');
-    downloadContent('flash_cards.csv', content);
+
+    console.log(blobbedCards)
+
+    const zip = new JSZip();
+    for (const card of blobbedCards) {
+      for (const [path, blob] of card.namedImageBlobs) {
+        zip.file(path, blob);
+      }
+      const [audioPath, audioBlob] = card.namedAudioBlob;
+      zip.file(audioPath, audioBlob);
+    }
+
+    const headerRow = ['Word', 'Picture', 'Gender, Personal Connection, Extra Info (Back side)', 'Pronunciation (Recording and/or IPA)', 'Test Spelling? (y = yes, blank = no)'];
+    const cardRows = blobbedCards.map(card => {
+      const imgs = card.namedImageBlobs.map(([path, b]) => `<img src="${path}">`).join('');
+      const [audioPath, b] = card.namedAudioBlob;
+      const sound = `[sound:${audioPath}]`;
+      const examples = card.examples.join(';');
+      return [card.word, imgs, examples, sound, ''].map(f => `"${f}"`);
+    });
+    const rows = [headerRow, cardRows];
+    const lines = rows.map(row => row.join(','));
+    const content = lines.join('\n');
+    zip.file('flash_cards.csv', content);
+
+    const zipBlob = await zip.generateAsync();
+    downloadContent('content.zip', zipBlob);
   }
 
   render() {
